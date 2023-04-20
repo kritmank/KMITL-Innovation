a0e612cba5c1bfae964810751cc0ded4022b5bf3
Krittitee Jongsaksawat <102571750+kritmank@users.noreply.github.com>
2023-04-20 21:09:47 +0700

    Update code
    - Change url to our php
    - Change method from GET to POST with json file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <WiFiClient.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <DHT.h>
@@ -13,54 +13,72 @@ const char* password = "12345678"; //WiFi Password
 int delayTime = 20 * 1000; //10s
 
 void setup() {
-    Serial.begin(9600);
-    dht.begin();
-    delay(10);
-    // We start by connecting to a WiFi network
-    Serial.println();
-    Serial.println();
-    Serial.print("Connecting to "); 
-    Serial.println(ssid); 
-    WiFi.begin(ssid, password); //Connected WiFi
-    while (WiFi.status() != WL_CONNECTED) { 
-      delay(500); 
-      Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.begin(9600);
+  dht.begin();
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to "); 
+  Serial.println(ssid); 
+  WiFi.begin(ssid, password); //Connected WiFi
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(500); 
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  Serial.println("---------------------");
+  Serial.println("--------- Start ------------");
 
   if (WiFi.status() == WL_CONNECTED) {
     float temp = dht.readTemperature();
     float humid = dht.readHumidity();
+    String temp_str = String(temp);
+    String humid_str = String(humid);
 
-    WiFiClient client;
     HTTPClient http;
-    String url = "http://api.thingspeak.com/update?api_key=0Q0VYIFXC41WJUDA";
-    url += "&field1=" + String(temp);
-    url += "&field2=" + String(humid);
-    Serial.println("URL : " + url);
-    Serial.println("Temperature : " + String(temp));
-    Serial.println("Humidity : " + String(humid));
-    http.begin(client, url);
-    // http.addHeader("Content-Type", "application/json");
+  // Set the URL of the server
+    http.begin("http://161.246.52.20/expo/import.php");
 
-    // String json = "{\"api_key\":\"0Q0VYIFXC41WJUDA\",\"field1\":\"" + String(x) + "\",\"field2\":\"" + String(y) + "\"}";
-    int httpResponseCode = http.POST("");
+    // Set the content type of the request
+    http.addHeader("Content-Type", "application/json");
+
+    // Create a JSON document
+    DynamicJsonDocument jsonDoc(1024);
+
+    // Add data to the JSON document
+    jsonDoc["deviceName"] = "device00";
+    jsonDoc["sensor00"] = temp_str;
+    jsonDoc["sensor01"] = humid_str;
+
+    Serial.println("Temperature : " + temp_str);
+    Serial.println("Humidity: " + humid_str);
+
+    // Serialize the JSON document to a string
+    String jsonString;
+    serializeJson(jsonDoc, jsonString);
+    // Send the POST request
+    int httpResponseCode = http.POST(jsonString);
+
+    // Check for errors
     if (httpResponseCode > 0) {
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
     } else {
-      Serial.print("Error code: ");
+      Serial.print("Error sending POST request: ");
       Serial.println(httpResponseCode);
     }
-    http.end();
-  }
 
-  delay(10000); 
+    // Free resources
+    http.end();
+
+    // Wait for some time before sending the next request
+    delay(1000);
+  }
+  delay(delayTime);
 }
